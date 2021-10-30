383e641d51ed957223fa7257ba264c6626891e03
João Manoel Neto <joaaomanooel@gmail.com>
2021-10-30 18:27:00 -0300

    feat: add status bar

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 import Home from '@/screens/Home';
+import { StatusBar } from 'react-native';
 
 export default () => {
-  return <Home />;
+  return (
+    <>
+      <StatusBar animated barStyle="dark-content" showHideTransition="slide" />
+      <Home />
+    </>
+  );
 };
