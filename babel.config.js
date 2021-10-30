d48717eed85fd238251bb74dc168c030f179c3b6
João Manoel Neto <joaaomanooel@gmail.com>
2021-10-30 12:24:45 -0300

    feat: create screens folder

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,10 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+module.exports = function babelConfig(api) {
+  api.cache(true);
+  return {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+      ['babel-plugin-styled-components'],
+      ['module-resolver', { alias: { '@/root': '.', '@': './src' } }],
+    ],
+  };
 };
