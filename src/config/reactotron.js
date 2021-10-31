24f086ae4d3356083a15543d8966b7b3f82090d9
João Manoel Neto <joaaomanooel@gmail.com>
2021-10-30 21:52:10 -0300

    chore: add reactotron

---
+++ src/config/reactotron.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-console */
+import Reactotron, {
+  asyncStorage,
+  trackGlobalErrors,
+  openInEditor,
+  overlay,
+  networking,
+} from 'reactotron-react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import sagaPlugin from 'reactotron-redux-saga';
+import { reactotronRedux } from 'reactotron-redux';
+
+const handleReactotron = () => {
+  const tron = Reactotron.configure({ name: 'umngani' })
+    .setAsyncStorageHandler(AsyncStorage)
+    .use(reactotronRedux())
+    .use(asyncStorage())
+    .use(sagaPlugin())
+    .use(overlay())
+    .use(trackGlobalErrors())
+    .use(openInEditor())
+    .use(networking())
+    .useReactNative({ asyncStorage: true })
+    .connect();
+
+  tron.clear();
+
+  console.tron = tron;
+  return tron;
+};
+
+const reactotron = __DEV__ ? handleReactotron() : undefined;
+
+export default reactotron;
